517c7719fb8071f19f2fe044f32eba03648d8358
Eberhard Beilharz <eb1@sil.org>
2011-03-15 15:28:08 +0100

    Another bugfix
    Change-Id: I4b827a35c0409c95bb5439a402dd101496c5b212

--- testproject/testprojectTests/Test.cs
+++ testproject/testprojectTests/Test.cs
@@ -1,8 +1,8 @@
 // Copyright (c) 2011, SIL International. All Rights Reserved.
 // <copyright from='2011' to='2011' company='SIL International'>
-// 	Copyright (c) 2011, SIL International. All Rights Reserved.
-// 	Distributable under the terms of either the Common Public License or the
-// 	GNU Lesser General Public License, as specified in the LICENSING.txt file.
+// Copyright (c) 2011, SIL International. All Rights Reserved.
+// Distributable under the terms of either the Common Public License or the
+// GNU Lesser General Public License, as specified in the LICENSING.txt file.
 // </copyright>
 using System;
 using NUnit.Framework;
