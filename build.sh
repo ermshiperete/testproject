12584117755135f0e7d6e0cf7115e494b4908668
Eberhard Beilharz <eb1@sil.org>
2021-07-08 10:37:17 +0200

    Add long line

--- build.sh
+++ build.sh
@@ -8,3 +8,5 @@ cd bld
 $FWROOT/nant/bin/nant -D:fwroot=$FWROOT testproject
 
 # Add comment
+
+# This is a long comment that will go so long that it will be over 130 characters so that we see how much GitHub will display in one line.
